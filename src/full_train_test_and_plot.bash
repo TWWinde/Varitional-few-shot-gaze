b405fb8c71c9ee61ae999d558007f6053d11c2ef
TWWinde <117169041+TWWinde@users.noreply.github.com>
2023-05-20 17:03:52 +0200

    Update full_train_test_and_plot.bash

--- src/full_train_test_and_plot.bash
+++ src/full_train_test_and_plot.bash
@@ -50,7 +50,7 @@ then
 		\
 		--normalize-3d-codes \
 		--embedding-consistency-loss-type angular \
-		--reconstruction-loss-type ReconstructionL1Loss \
+		--reconstruction-loss-type AlexLoss \
 		--backprop-gaze-to-encoder \
 		\
 		--num-data-loaders 16 \
