3627533d89c09a4142f810c06d35fc4fe6a2899a
TWWinde <117169041+TWWinde@users.noreply.github.com>
2023-05-22 20:41:03 +0200

    Update kl_loss.py

--- src/losses/kl_loss.py
+++ src/losses/kl_loss.py
@@ -3,7 +3,7 @@ import torch
 
 class KLDivergenceLoss(object):
 
-    def __call__(self, output_dict):
+    def __call__(self, input_dict, output_dict):
         kl_loss = -0.5 * torch.sum(1 + output_dict['logvar'] - output_dict['mu'].pow(2) - output_dict['logvar'].exp(),dim=1)
         kl_loss = torch.mean(kl_loss)
         return kl_loss
